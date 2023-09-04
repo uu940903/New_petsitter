dd4738201473418c3f19dbf7f590161e885a1e79
losscut25 <leehuimin13@gmail.com>
2023-09-04 17:57:05 +0900

    reupdate

---
+++ src/main/java/com/pet/sitter/member/dto/UserDTO.java
@@ -0,0 +1,36 @@
+package com.pet.sitter.member.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //기본키
+
+    private String userid;
+    private String name; //유저 이름
+    private String password; //유저 비밀번호
+    private String email; //유저 구글 이메일
+    private String role; //유저 권한 (일반 유저, 관리자)
+
+    @Builder
+    public UserDTO(String userid, String name, String password, String email, String role) {
+        this.userid=userid;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public UserDTO() {
+
+    }
+}
