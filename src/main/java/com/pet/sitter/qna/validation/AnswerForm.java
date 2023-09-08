dd99f0328c568cd57d7aa4f4f0ddc80b4df45a51
orchidHYE <trolly00@naver.com>
2023-09-08 14:33:09 +0900

    댓글까지완.

---
+++ src/main/java/com/pet/sitter/qna/validation/AnswerForm.java
@@ -0,0 +1,28 @@
+package com.pet.sitter.qna.validation;
+
+import com.pet.sitter.common.entity.Member;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class AnswerForm {
+
+    @NotEmpty(message = "내용을 입력해주세요.")
+    private String content;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+    private Long id;
+
+    private Member member;
+
+
+
+
+
+
+}
