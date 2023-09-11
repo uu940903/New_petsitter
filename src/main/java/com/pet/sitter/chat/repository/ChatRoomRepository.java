682179372584b6004cbe72e281790079df9b8112
favud20 <favud20@gmail.com>
2023-09-11 09:50:54 +0900

    본인 게시글 채팅방 생성 차단, 중복 채팅방 생성 차단, kakao.css 적용

--- src/main/java/com/pet/sitter/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/pet/sitter/chat/repository/ChatRoomRepository.java
@@ -2,6 +2,7 @@ package com.pet.sitter.chat.repository;
 
 import com.pet.sitter.chat.dto.ChatRoomDTO;
 import com.pet.sitter.common.entity.ChatRoom;
+import com.pet.sitter.common.entity.Petsitter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,8 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     ChatRoom findChatRoomById(Long id);
 
     ChatRoom findChatRoomByRoomUUID(String roomUUID);
+
+    ChatRoom findChatRoomByPetsitter(Petsitter petsitter);
+
+    ChatRoom findChatRoomByPetsitterAndGuestId(Petsitter petsitter, String memberId);
 }
