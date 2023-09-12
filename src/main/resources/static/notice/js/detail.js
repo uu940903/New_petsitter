ba562a975dee8c593429091234e33d7b6dfef4ec
favud20 <favud20@gmail.com>
2023-09-12 11:33:56 +0900

    파일 정리

--- src/main/resources/static/notice/js/detail.js
+++ src/main/resources/static/notice/js/detail.js
@@ -1,5 +1,5 @@
-$$(document).ready(function() {
-    $("#deleteButton").click(function(e) {
+$$(document).ready(function () {
+    $("#deleteButton").click(function (e) {
         e.preventDefault();
 
         var noNo = $(this).data("no"); // 데이터 속성에서 값을 가져옵니다.
@@ -11,13 +11,13 @@ $$(document).ready(function() {
                 data: {
                     _method: "delete"
                 },
-                success: function(response) {
+                success: function (response) {
                     // 성공적으로 삭제되었을 때 할 작업을 여기에 추가하세요.
                     alert("삭제되었습니다.");
                     // 페이지 리로드 또는 필요한 작업 수행
                     location.reload();
                 },
-                error: function(err) {
+                error: function (err) {
                     // 삭제 중 오류 발생 시 처리할 작업을 여기에 추가하세요.
                     alert("삭제 중 오류가 발생했습니다.");
                 }
